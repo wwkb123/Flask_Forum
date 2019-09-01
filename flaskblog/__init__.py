6cb4079ba7aa0a851c5028b35162e1bdbc012d34
wwkb123 <wwkb1233@gmail.com>
2019-09-01 14:59:09 -0400

    add custom error pages

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -18,17 +18,21 @@ mail = Mail()
 def create_app(config_class=Config):
 	app = Flask(__name__)
 	app.config.from_object(Config)
+
 	db.init_app(app)
 	bcrypt.init_app(app)
 	login_manager.init_app(app)
 	mail.init_app(app)
+
 	# from flaskblog import routes
 	from flaskblog.users.routes import users
 	from flaskblog.posts.routes import posts
 	from flaskblog.main.routes import main
+	from flaskblog.errors.handlers import errors
 
 	app.register_blueprint(users)
 	app.register_blueprint(posts)
 	app.register_blueprint(main)
-
+	app.register_blueprint(errors)
+	
 	return app
