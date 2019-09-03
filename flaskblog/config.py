7ff66710d9f3b73dd6c8a3829f909f2797166b77
Yiu Chung Yau <yiuchungyau@gmail.com>
2019-09-03 03:17:37 -0400

    migrate database to PostgreSQL

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,7 +4,6 @@ import psycopg2
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY')  #'a99c515275ef1355c18ff3efc46221ce'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  #'sqlite:///site.db'
-	conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
